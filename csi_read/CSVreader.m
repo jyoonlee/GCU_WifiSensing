8828f90f583439041c0e6735e4db34be440dc294
jyoonlee <2jyoons@naver.com>
2020-09-25 16:10:06 +0900

    new_version

--- csi_read/CSVreader.m
+++ csi_read/CSVreader.m
@@ -1,39 +1,62 @@
 
-% i = 1;
+csi_trace = read_bf_file('csi_clear.dat');
+% %csi_trace = read_bf_file('sample_data/log.all_csi.6.7.6');
+
+% % 
+csi_entry = csi_trace {1};
+% %csi_entry1 = csi_trace {7};
+csi = get_scaled_csi (csi_entry)
+abs(csi)
+
+db(abs(csi));
+get_eff_SNRs(csi);
+%b = abs(reshape(csi, [],30).')
 % 
-% T = [];
-% V = [];
-% A1 = zeros(1,3,30); %csv를 담을 유사 A
+% 
+% 
+% 
+% %여기서부터
+ plot(db(abs(reshape(csi, [],30).')));
+ legend('RX Antenna Ax1', 'RX Antenna Ax2', 'RX Antenna Bx1','RX Antenna Bx2', 'RX Antenna Cx1', 'RX Antenna Cx2', 'Location', 'SouthEast' );
+ xlabel('Subcarrier index');
+ ylabel('SNR [dB]');
+% % 
+db(get_eff_SNRs(csi), 'pow');
+
+
+
+% 
+% csi_entry = csi_trace{20}
+% 
+% csi = get_scaled_csi(csi_entry);
+% db(get_eff_SNRs(csi), 'pow')
+%여기까지
+
+ %i = 1;
+% 
+ %T = [];
+ %V = [];
+ %A1 = zeros(1,3,30) %csv를 담을 유사 A 현재 3x1로 구성
 %csi_trace = read_bf_file('C:\Users\ACA\Documents\MATLAB\matlab\real_data\walking10.data');
 %csi_trace = csvread('C:\Users\ACA\PycharmProjects\UnionProject\2018_05_09_walk10_04_delay1000.csv',0,1,[0,1,10000,90]);
-csi_trace = read_bf_file('csi.dat');
-%csi_trace = read_bf_file('sample_data/log.all_csi.6.7.6');
-csi_entry = csi_trace {1};
+%csi_trace = read_bf_file('csi_6.dat')
+%csi_trace = read_bf_file('test.dat')
 
-csi = get_scaled_csi (csi_entry);
+%csi_entry = csi_trace{3}
+%csi_entry = csi_trace{1}
 
-plot(db(abs(squeeze(csi).')))
-legend('RX Antenna A', 'RX Antenna B', 'RX Antenna C', 'Location', 'SouthEast' );
-xlabel('Subcarrier index');
-ylabel('SNR [dB]');
-
-db(get_eff_SNRs(csi), 'pow')
-
-csi_entry = csi_trace{20}
-
-csi = get_scaled_csi(csi_entry);
-db(get_eff_SNRs(csi), 'pow')
-% while(i<1000)
+%반복하면서 담음
+%while(i<1000)
 % 
-% %csi_entry = csi_trace{i};
-% %csi = get_scaled_csi(csi_entry);
-% 
+%csi_entry = csi_trace{i};
+%csi = get_scaled_csi(csi_entry);
+% 이상함 이부분
 % A1(:,1,:) = csi_trace(i,1:30);
 % A1(:,2,:) = csi_trace(i,31:60);
 % A1(:,3,:) = csi_trace(i,61:90);
-% 
-% csi_entry2 =  csi_trace2{i};
-% csi2 = get_scaled_csi(csi_entry2);
+% %%
+%csi_entry2 =  csi_trace2{i};
+%csi2 = get_scaled_csi(csi_entry2);
 % 
 % 
 % A = A1;
@@ -78,6 +101,6 @@ db(get_eff_SNRs(csi), 'pow')
 % 
 % i = i + 1;
 % 
-% end
+%end
 
 
--- csi_read/get_scaled_csi.m
+++ csi_read/get_scaled_csi.m
@@ -4,24 +4,34 @@
 %
 function ret = get_scaled_csi(csi_st)
     % Pull out CSI
+    %csi는 복소수이다.
     csi = csi_st.csi;
-
+    
+    
     % Calculate the scale factor between normalized CSI and RSSI (mW)
+    %conj는 켤레 복소수를 구한다. 켤레 복소수와 복소수를 곱해 복소수를 실수로 만듬
+    %2 - 2i * 2 + 2i = 8 과 같이
     csi_sq = csi .* conj(csi);
+    %csi_sq(:)는 일열로 만든 후 모든 값을 더함
     csi_pwr = sum(csi_sq(:));
+    %dbinv는 10.^(x/10)을 반환 데시벨을 변환함
+    %rss값을 데시벨을 변환한 후 더한 다음 다시 'pow'를 이용해 데시벨로 바꿔준 후 agc와 44를 빼줌 
     rssi_pwr = dbinv(get_total_rss(csi_st));
     %   Scale CSI -> Signal power : rssi_pwr / (mean of csi_pwr)
+    % subcarrier수인 30개로 나누면 평균 CSI_pwr값이 나옴 그값을 rssi_pwr에 나눔
     scale = rssi_pwr / (csi_pwr / 30);
 
     % Thermal noise might be undefined if the trace was
     % captured in monitor mode.
     % ... If so, set it to -92
+    % noise가 -127보다낮을 경우 -92로 설정 아닐 경우 기존 noise사용
     if (csi_st.noise == -127)
         noise_db = -92;
     else
         noise_db = csi_st.noise;
     end
     thermal_noise_pwr = dbinv(noise_db);
+
     
     % Quantization error: the coefficients in the matrices are
     % 8-bit signed numbers, max 127/-128 to min 0/1. Given that Intel
@@ -31,6 +41,7 @@ function ret = get_scaled_csi(csi_st)
     % The total power is then 1^2 = 1 per entry, and there are
     % Nrx*Ntx entries per carrier. We only want one carrier's worth of
     % error, since we only computed one carrier's worth of signal above.
+    % 이와 같은 공식으로 error를 구함
     quant_error_pwr = scale * (csi_st.Nrx * csi_st.Ntx);
 
     % Total noise and error power
