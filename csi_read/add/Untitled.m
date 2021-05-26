530e8b32fc0b0e1d56c2be298a9f031e7e2d9b57
yeocy <ducksdud08@naver.com>
2021-05-26 19:35:09 +0900

    CSI Tool
    Wi-Fi Sensing using CSI Tool

--- csi_read/add/Untitled.m
+++
@@ -1,3 +0,0 @@
-i = '4';
-a = [1,3,3];
-csvwrite('aaa.csv' + i,a);
--- csi_read/추가/sanitize_phase.m
+++
@@ -1,8 +0,0 @@
-% Phase Transformation
-function output_phase=sanitize_phase(input_phase)
-
-x = unwrap(input_phase);
-a = ( x(30) - x(1) )/ 30;
-b = sum(x)/30;
-range = [-15:-1 , 1:15];
-output_phase = x - (a*range + b*ones(1,30));
